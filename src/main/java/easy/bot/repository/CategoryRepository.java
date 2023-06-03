cfc77f890c38b286d928d2058e6884b566452fb7
Yevhenii Kuzmenko <zheniakuzma@gmail.com>
2023-06-03 16:11:06 +0300

    Added RESTFul HTTP

---
+++ src/main/java/easy/bot/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package easy.bot.repository;
+
+import easy.bot.entity.Category;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+
+}
--- src/main/java/easy/bot/repository/ItemRepository.java
+++ src/main/java/easy/bot/repository/ItemRepository.java
@@ -1,9 +1,15 @@
 package easy.bot.repository;
 
+import easy.bot.entity.Category;
 import easy.bot.entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ItemRepository extends CrudRepository<Item, Long> {
+    List<Item> findAllByCategory(Optional<Category> category);
 }
